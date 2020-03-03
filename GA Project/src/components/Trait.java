b5e0db4a338551493853e9167d7606c6c588869c
jgross11 <jgross11@ycp.edu>
2020-03-02 23:56:29 -0500

    Created class for a numerical-valued trait

---
+++ GA Project/src/components/Trait.java
@@ -0,0 +1,12 @@
+package components;
+
+/**
+ * @author jgross11@ycp.edu
+ *
+ * Class that represents a specific trait that a specimen contains. 
+ */
+public class Trait {
+	private String name;
+	private float minVal;
+	private float maxVal;
+}
