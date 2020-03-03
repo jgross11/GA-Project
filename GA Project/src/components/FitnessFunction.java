a33b6d04dfc119579615fc2b8b125196da80815a
jgross11 <jgross11@ycp.edu>
2020-03-02 23:55:59 -0500

    Created container class that holds fitness function

---
+++ GA Project/src/components/FitnessFunction.java
@@ -0,0 +1,14 @@
+package components;
+
+/**
+ * @author jgross11@ycp.edu
+ * 
+ * Class representing a generic fitness function that calculates a given specimen's fitness 
+ */
+public class FitnessFunction {
+	
+	
+	public void calculateFitness(Specimen s) {
+		throw new UnsupportedOperationException("### MUST OVERRIDE DEFAULT FITNESS FUNCTION ###");
+	}
+}
